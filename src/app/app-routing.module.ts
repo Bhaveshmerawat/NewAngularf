029c0e798707804834cba59bce94d67f5321c96d
bhaveshmerawat <Bhaveshm@shriffle.com>
2021-09-08 15:50:43 +0530

    remove nav

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  // { path: '', redirectTo: '/home', pathMatch: 'full' },
+  // { path: 'home', component: AppComponent}
 ];
 
 @NgModule({
